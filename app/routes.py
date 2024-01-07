25091b904fb983f4dd31fd821b96d3896177e790
Gabe Solomon <gabe@rinsed.co>
2024-01-07 14:45:16 -0500

    adding to standings.html

--- app/routes.py
+++ app/routes.py
@@ -25,8 +25,9 @@ def standings():
             User.profile_picture,
             func.sum(Game.team_win.cast(Integer)).label('overall_wins'),
             func.count(distinct(Game.gameid)).label('overall_games'),
-            func.count(distinct(Game.gameid)) - func.sum(Game.team_win).label('overall_losses'),
-            func.sum(Game.team_win.cast(Integer)) / func.count(distinct(Game.gameid)).label('win_pct')
+            (func.count(distinct(Game.gameid)).cast(Integer)  -
+                func.sum(Game.team_win.cast(Integer))).label('overall_losses'),
+            func.round((func.sum(Game.team_win.cast(Integer)) / func.count(distinct(Game.gameid))),3).label('win_pct')
         )
         .join(Team, Game.team_id == Team.id)
         .join(Squad, Team.id == Squad.team_id)
@@ -35,10 +36,47 @@ def standings():
     )
     # convert to dictionary
     squad_records = query.all()
-
+    sql = str(query)
+    print(sql)
+    
     return render_template('standings.html', title='Standings', 
         users=users, teams=teams, squads=squads, games=games, squad_names=squad_names,
         squad_records=squad_records)
+
+@app.route('/monthly_standings')
+def monthly_standings():
+    # get users, teams, squads, squad names, and games
+    users = User.query.all()
+    teams = Team.query.all()
+    squads = Squad.query.all()
+    games = Game.query.all()
+    squad_names = SquadName.query.all()
+
+    # use sqlalchemy to calculate the team records, by calendar month
+    # group by the calendar month of the game date
+    query = (
+        db.session.query(
+            User.id,
+            User.username,
+            User.profile_picture,
+            func.month(Game.date).label('month'),
+            func.sum(Game.team_win.cast(Integer)).label('monthly_wins'),
+            func.count(distinct(Game.gameid)).label('monthly_games'),
+            func.count(distinct(Game.gameid)) - func.sum(Game.team_win).label('monthly_losses'),
+            func.sum(Game.team_win.cast(Integer)) / func.count(distinct(Game.gameid)).label('monthly_win_pct')
+        )
+        .join(Team, Game.team_id == Team.id)
+        .join(Squad, Team.id == Squad.team_id)
+        .join(User, Squad.user_id == User.id)
+        .group_by(User.id, User.username, User.profile_picture, func.month(Game.date))
+    )
+
+    # convert to dictionary
+    monthly_squad_records = query.all()
+
+    return render_template('monthly_standings.html', title='Monthly Standings', 
+        users=users, teams=teams, squads=squads, games=games, squad_names=squad_names,
+        monthly_squad_records=monthly_squad_records)
 
 @app.route('/create_user', methods=['GET', 'POST'])
 def register():
